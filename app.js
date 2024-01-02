7a1dd1c48ad536da206a17cda6ce89c2fd02bdbb
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-01-02 12:16:57 +0100

    I modified app.js

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ const blurring = () => {
   }
 
   loadingText.innerHTML = `${load}%`;
+  backgroundBg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+};
+
+// stackoverflow help!!
+//https://stackoverflow.com/questions/10756313/javascrpt-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
 
 let int = setInterval(blurring, 30);
