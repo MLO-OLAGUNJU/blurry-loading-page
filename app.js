b76665bbfc0c9bf3fe8130fa25bf1baa1cf1923e
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-01-02 11:52:07 +0100

    I added more script to app.js

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const loadingText = document.querySelector(".loading-text");
 const backgroundBg = document.querySelector(".bg");
 
 let load = 0;
+
+const blurring = () => {
+  load++;
+
+  console.log(load);
+};
