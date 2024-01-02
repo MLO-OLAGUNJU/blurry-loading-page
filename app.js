8ef3d177932937d8d88563c782d9a94999014f39
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-01-02 15:35:25 +0100

    made some edits to perfect it

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-const loadingText = document.querySelector(".loading-text");
-const backgroundBg = document.querySelector(".bg");
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
 
 let load = 0;
 
-const blurring = () => {
+let int = setInterval(blurring, 30);
+
+function blurring() {
   load++;
 
   if (load > 99) {
     clearInterval(int);
   }
 
-  loadingText.innerHTML = `${load}%`;
-  backgroundBg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
-};
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 120, 1, 0.1);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
 
-// stackoverflow help!!
-//https://stackoverflow.com/questions/10756313/javascrpt-jquery-map-a-range-of-numbers-to-another-range-of-numbers
-
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
-
-let int = setInterval(blurring, 30);
