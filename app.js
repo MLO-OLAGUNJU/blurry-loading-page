be0a91710e3c6d98b1d1425be83ab5fd536fcbc4
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-01-02 11:58:38 +0100

    I modified app.js

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const backgroundBg = document.querySelector(".bg");
 
 let load = 0;
 
-let int = setInterval(blurring, 30);
-
 const blurring = () => {
   load++;
 
   if (load > 99) {
     clearInterval(int);
   }
+
+  loadingText.innerHTML = `${load}%`;
 };
+
+let int = setInterval(blurring, 30);
